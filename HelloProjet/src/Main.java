3cce65a7822f6e93e22b7ff073a2862fb6b9b7f0
zhanglj1988 <3466417782@qq.com>
2019-02-26 15:19:53 +0800

    小伙子

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-		System.out.println("111111111");
+		System.out.println("21111122222");
 		System.out.println("5555555555");
+		小伙子,有前途!!! 江哥看好你 !
     }
 }
