1e16f4a5f8fc1eeb1777ece27abbe828aed7b285
zhanglj1988 <3466417782@qq.com>
2019-02-26 14:48:40 +0800

    赵六修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+		System.out.println("2222222222");
 		System.out.println("111111111");
     }
 }
